b837acf64c6348c9a59cb903d046e52ceb18d9c7
yyokii <yyokii.h@gmail.com>
2021-05-30 15:29:53 +0900

    Initial Commit

---
+++ DemoMetal/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DemoMetal
+//
+//  Created by Higashihara Yoki on 2021/05/30.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DemoMetal/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DemoMetal
+//
+//  Created by Higashihara Yoki on 2021/05/30.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
